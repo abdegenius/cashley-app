5260de1336238d41fb7489a1431c24c8d67ca05e
tali-creator <talinanzing111@gmail.com>
2025-10-20 00:03:17 +0100

    added utility

---
+++ src/app/app/airtime/page.tsx
@@ -0,0 +1,10 @@
+import Purchase from '@/components/flows/service-flow'
+import React from 'react'
+
+export default function Airtime() {
+  return (
+    <div>
+    <Purchase type='airtime'/>
+    </div>
+  )
+}
