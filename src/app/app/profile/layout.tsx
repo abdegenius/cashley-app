d1a885528f3b47a0f3d77addbf9314d7853f9b58
tali-creator <talinanzing111@gmail.com>
2025-10-19 21:38:14 +0100

    added deposite

--- src/app/app/profile/layout.tsx
+++ src/app/app/profile/layout.tsx
@@ -1,5 +1,5 @@
 import React, { Children, ReactNode } from "react";
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
-  return <div className="w-full min-h-screen flex flex-col">{children}</div>;
+  return <div className="w-full h-[89vh] ">{children}</div>;
 }
