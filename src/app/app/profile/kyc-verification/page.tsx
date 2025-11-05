851abbe450b5c200006dae829abe58d16369869d
tali-creator <talinanzing111@gmail.com>
2025-11-05 22:06:55 +0100

    added verification flow

--- src/app/app/profile/kyc-verification/page.tsx
+++ src/app/app/profile/kyc-verification/page.tsx
@@ -4,15 +4,73 @@ import Button from "@/components/ui/Button";
 import { Section } from "@/components/ui/Section";
 import { Check } from "lucide-react";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
+import api from "@/lib/axios";
+import { ApiResponse } from "@/types/api";
+import { formatToNGN } from "@/utils/amount";
+
+interface Limits {
+  level: number;
+  per_single_transfer: number;
+  daily_transaction_limit: number;
+  wallet_balance: string;
+  requires: string[];
+}
 
 export default function KYCVerification() {
-  const [step, setStep] = useState(1);
-  const { user } = useAuth()
-  console.log("user", user)
+  const [limits, setlimits] = useState<Limits[]>([]);
+  const { user } = useAuth();
 
-  // const ren
+  const fetchLimits = async () => {
+    try {
+      const res = await api.get<ApiResponse>("/limits");
+      if (!res.data.error) setlimits(res.data.data);
+    } catch {
+      console.log("failed to fetch");
+    }
+  };
+
+  useEffect(() => {
+    fetchLimits();
+  }, []);
+
+  const step1 = user?.bvn;
+  const step2 = step1 && user.nin;
+  const step3 =
+    step2 &&
+    user.city &&
+    user.country &&
+    user.house_number &&
+    user.street &&
+    user.state;
+
+  const isStepIncomplete = (count: number) => {
+    switch (count) {
+      case 1:
+        return !step1;
+      case 2:
+        return step1 && !step2;
+      case 3:
+        return step2 && !step3;
+      default:
+        return false;
+    }
+  };
+
+  const links = (id: number) => {
+    switch (id) {
+      case 1:
+        return "bvn";
+      case 2:
+        return "nin";
+      case 3:
+        return "address";
+      case 4:
+        return "cac";
+    }
+  };
+
   return (
     <div className="flex flex-col gap-10 h-full max-w-lg mx-auto overflow-y-scroll w-full px-4">
       <div>
@@ -35,80 +93,68 @@ export default function KYCVerification() {
         <div className="py-2">Verify to use Cashley</div>
         <div className="w-full h-1 bg-white rounded-full" />
         <div className="flex  py-2 gap-3 text-sm items-center">
-          <span>0</span>
-          out of 3 tiers completed
+          <span></span>
+          {user?.level ?? 0} out of 4 tiers completed
         </div>
       </div>
 
       <Section title="Verification Tiers" description="" delay={0.12}>
-         <div className=" flex w-full flex-col gap-3 pb-5">
-          <div className="flex w-full justify-between items-center  p-2">
-            <div className="flex gap-3 items-center">
-              <Image
-                src={"/svg/vector1.svg"}
-                alt="vector1"
-                width={25}
-                height={25}
-              />
-              <div>
-                <h1 className="font-bold">Tier 1 - Standard</h1>
-                <h1 className="text-sm">In Progress</h1>
+        {limits?.map((limit) => (
+          <div key={limit.level} className=" flex w-full flex-col gap-3 pb-5">
+            <div className="flex w-full justify-between items-center  p-2">
+              <div className="flex gap-3 items-center">
+                <Image
+                  src={"/svg/vector1.svg"}
+                  alt="vector1"
+                  width={25}
+                  height={25}
+                />
+                <div>
+                  <h1 className="font-bold">Level {limit.level} </h1>
+                  <h1 className="text-sm">
+                    {limit.level <= Number(user?.level)
+                      ? "Completed"
+                      : "In progress"}
+                  </h1>
+                </div>
+              </div>
+              <div className="flex flex-col placeholder-text gap-2 items-center">
+                <span className="flex">
+                  {" "}
+                  <Check size={20} /> Requires
+                </span>
+                <div className="w-full max-w-40 justify-end flex flex-wrap gap-2">
+                  {limit.requires.map((rec, idx) => (
+                    <span key={idx}>{rec.toUpperCase()}</span>
+                  ))}
+                </div>
               </div>
             </div>
-            <div className="px-8 py-2 rounded-full font-semibold bg-card">
-              ₦50k /month
-            </div>
-          </div>
-          <div className="flex placeholder-text gap-2 items-center">
-            <Check size={20} />
-            <span>BVN verification</span>
-          </div>
-
-          {step === 1 && (
-            <Button
-              type="primary"
-              width="w-full max-w-2xl py-4"
-              text="Start Tier 3 Verification"
-              href="/app/profile/kyc-verification/bvn"
-            />
-          )}
-        </div>
-        
-        <div className=" flex w-full flex-col gap-3 pb-5 ">
-          <div className="flex w-full justify-between items-center  p-2">
-            <div className="flex gap-3 items-center">
-              <Image
-                src={"/svg/vector1.svg"}
-                alt="vector1"
-                width={25}
-                height={25}
-              />
-              <div>
-                <h1 className="font-bold">Tier 2 - Standard</h1>
-                <h1 className="text-sm">In Progress</h1>
+            <div className="flex items-center  text-center justify-between rounded-full px-8 py-2 bg-card gap-4 ">
+              <div className=" text-sm   ">
+                <div className="placeholder-text ">Daily limit</div>
+                {formatToNGN(limit.daily_transaction_limit)}
+              </div>
+              <div className=" text-sm  ">
+                <div className="placeholder-text ">Single Transfer</div>
+                {formatToNGN(limit.per_single_transfer)}
+              </div>
+              <div className=" text-sm  ">
+                <div className="placeholder-text ">Balance</div>
+                {limit.wallet_balance}
               </div>
             </div>
-            <div className="px-8 py-2 rounded-full font-semibold bg-card">
-              ₦50k/month
-            </div>
-          </div>
-          <div className="flex placeholder-text gap-2 items-center">
-            <Check size={20} />
-            <span>NIN verification</span>
-          </div>
 
-          {step === 1 && (
-            <Button
-              type="primary"
-              width="w-full max-w-2xl py-4"
-              text="Start Tier 1 Verification"
-              href="/app/profile/kyc-verification/nin"
-            />
-          )}
-        </div>
-       
-
-       
+            {isStepIncomplete(limit.level) && (
+              <Button
+                type="primary"
+                width="w-full max-w-2xl py-4"
+                text={`Start level ${limit.level} verification`}
+                href={`/app/profile/kyc-verification/${links(limit.level)} `}
+              />
+            )}
+          </div>
+        ))}
       </Section>
 
       <Section title="Tier 3 Benefits" description="" delay={0.12}>
