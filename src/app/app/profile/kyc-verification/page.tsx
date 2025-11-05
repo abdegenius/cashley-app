2f599d057be8718cf9b7ed847ef3e15d4a588bf9
tali-creator <talinanzing111@gmail.com>
2025-11-05 18:58:58 +0100

    added view transaction

--- src/app/app/profile/kyc-verification/page.tsx
+++ src/app/app/profile/kyc-verification/page.tsx
@@ -41,6 +41,39 @@ export default function KYCVerification() {
       </div>
 
       <Section title="Verification Tiers" description="" delay={0.12}>
+         <div className=" flex w-full flex-col gap-3 pb-5">
+          <div className="flex w-full justify-between items-center  p-2">
+            <div className="flex gap-3 items-center">
+              <Image
+                src={"/svg/vector1.svg"}
+                alt="vector1"
+                width={25}
+                height={25}
+              />
+              <div>
+                <h1 className="font-bold">Tier 1 - Standard</h1>
+                <h1 className="text-sm">In Progress</h1>
+              </div>
+            </div>
+            <div className="px-8 py-2 rounded-full font-semibold bg-card">
+              ₦50k /month
+            </div>
+          </div>
+          <div className="flex placeholder-text gap-2 items-center">
+            <Check size={20} />
+            <span>BVN verification</span>
+          </div>
+
+          {step === 1 && (
+            <Button
+              type="primary"
+              width="w-full max-w-2xl py-4"
+              text="Start Tier 3 Verification"
+              href="/app/profile/kyc-verification/bvn"
+            />
+          )}
+        </div>
+        
         <div className=" flex w-full flex-col gap-3 pb-5 ">
           <div className="flex w-full justify-between items-center  p-2">
             <div className="flex gap-3 items-center">
@@ -73,71 +106,9 @@ export default function KYCVerification() {
             />
           )}
         </div>
-        <div className=" flex w-full flex-col gap-3 pb-5">
-          <div className="flex w-full justify-between items-center  p-2">
-            <div className="flex gap-3 items-center">
-              <Image
-                src={"/svg/vector1.svg"}
-                alt="vector1"
-                width={25}
-                height={25}
-              />
-              <div>
-                <h1 className="font-bold">Tier 2 - Standard</h1>
-                <h1 className="text-sm">In Progress</h1>
-              </div>
-            </div>
-            <div className="px-8 py-2 rounded-full font-semibold bg-card">
-              ₦500k/month
-            </div>
-          </div>
-          <div className="flex placeholder-text gap-2 items-center">
-            <Check size={20} />
-            <span>Selfie verification</span>
-          </div>
+       
 
-          {step === 2 && (
-            <Button
-              type="primary"
-              width="w-full max-w-2xl py-4"
-              text="Start Tier 2 Verification"
-              href="/app/profile/kyc-verification/selfie-verification"
-            />
-          )}
-        </div>
-
-        <div className=" flex w-full flex-col gap-3 pb-5">
-          <div className="flex w-full justify-between items-center  p-2">
-            <div className="flex gap-3 items-center">
-              <Image
-                src={"/svg/vector1.svg"}
-                alt="vector1"
-                width={25}
-                height={25}
-              />
-              <div>
-                <h1 className="font-bold">Tier 2 - Standard</h1>
-                <h1 className="text-sm">In Progress</h1>
-              </div>
-            </div>
-            <div className="px-8 py-2 rounded-full font-semibold bg-card">
-              ₦2m /month
-            </div>
-          </div>
-          <div className="flex placeholder-text gap-2 items-center">
-            <Check size={20} />
-            <span>BVN verification</span>
-          </div>
-
-          {step === 3 && (
-            <Button
-              type="primary"
-              width="w-full max-w-2xl py-4"
-              text="Start Tier 3 Verification"
-              href="/app/profile/kyc-verification/bvn"
-            />
-          )}
-        </div>
+       
       </Section>
 
       <Section title="Tier 3 Benefits" description="" delay={0.12}>
