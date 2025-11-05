2d718d01ce1fc1880298b01d8227fdd2e961868f
abdegenius <johnnieabijah2000@gmail.com>
2025-11-05 17:09:09 +0100

    Major design updates

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,20 +1,18 @@
 
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 export const LoadingOverlay = () => (
-    <div className="fixed w-full h-full bg-card">
-        <AnimatePresence>
+    <div className="fixed inset-0 w-full h-full max-w-xl mx-auto flex flex-col items-center justify-center z-[99]">
+        <div className="absolute w-full h-full bg-stone-900/40 backdrop-blur-sm" />
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
             <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="fixed max-w-xl mx-auto w-full h-full m-0 p-0 z-50 inset-0 bg-stone-900/40 backdrop-blur-sm flex items-center justify-center"
-            >
-                <motion.div
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
-                    className="w-20 h-20 border-6 border-purple-500 border-t-transparent rounded-full"
-                />
-            </motion.div>
-        </AnimatePresence>
+                animate={{ rotate: 360 }}
+                transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+                className="w-20 h-20 border-6 border-purple-500 border-t-transparent rounded-full"
+            />
+        </motion.div>
     </div>
 );
